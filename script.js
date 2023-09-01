667fec267a777bd214ad77c4d3be89cad29f8926
enigma9119 <enigma9119@users.noreply.github.com>
2023-08-31 20:39:51 -0700

    Generalize the square width calculation

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const container = document.querySelector(".container");
-
 const NUM_SQUARES_PER_ROW_AND_COLUMN = 16;
+
+const container = document.querySelector(".container");
 
 function createGrid(dimensions = NUM_SQUARES_PER_ROW_AND_COLUMN) {
   for (let i = 0; i < dimensions; ++i) {
     for (let j = 0; j < dimensions; ++j) {
       const square = document.createElement("div");
 
-      // Each square should fill up 6.25% of the smaller window dimension.
+      // Each square should fill up an equal proportion of the smaller window dimension.
       // This ensures that the grid fits in the window without a need to scroll.
       if (window.innerWidth < window.innerHeight) {
         container.style.flexDirection = "row";
@@ -15,7 +15,8 @@ function createGrid(dimensions = NUM_SQUARES_PER_ROW_AND_COLUMN) {
         container.style.flexDirection = "column";
       }
 
-      square.style.flexBasis = "6.25%";
+      const squareWidth = 100 / dimensions;
+      square.style.flexBasis = `${squareWidth}%`;
       square.style.aspectRatio = "1 / 1";
       square.style.backgroundColor = "honeydew";
       square.style.border = "1px solid black";
